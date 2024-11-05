5b9477a98ee13b710808fcabebba06bf8454ea62
Dimitris Doukas <dd2040@hw.ac.uk>
2024-11-05 01:30:45 +0000

    After log in jwt token is stored in cookies now

--- src/api/auth.js
+++ src/api/auth.js
@@ -66,7 +66,7 @@ router.post("/login", (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      maxAge: 3600000, // 1 hour
+      maxAge: 24 * 60 * 60 * 1000 // 1 day
     });
 
     res.json({ message: "Login successful" });

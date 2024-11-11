ae8829abfe1a4910515b0edb5dd46279933d8f92
Dimitris Doukas <dd2040@hw.ac.uk>
2024-11-11 11:13:39 +0000

    created this to make an api request to sign user out

---
+++ public/js/signout.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    console.log("Frontend JavaScript loaded.");
+  
+    $("#signOutButton").on("click", async function (e) {
+        e.preventDefault();
+  
+        const username = $("#loginUsername").val();
+        const password = $("#loginPassword").val();
+  
+        $.ajax({
+        url: `api/auth/logout`,
+        method: "POST",
+        success: function (xhr) {
+          alert("Login successful!");
+          window.location.href = "/log-in"; // Redirect to homepage after login
+        },
+        error: function (xhr) {
+            alert("Can not log user out");
+        },
+      });
+    });
+  });
